3dcb3aa1af00683f4c8fb81f55c166dd889ee584
Sagar Kirtakar <sagarkirtakar2002@gmail.com>
2025-11-22 06:58:33 +0530

    edit main.java

--- src/com/soft/java/main.java
+++ src/com/soft/java/main.java
@@ -3,5 +3,7 @@ package com.soft.java;
 public class main {
     public static void main(String[] args) {
         System.out.println("Hello java programming..!");
+
+        System.out.println("Sagar Kirtakar");
     }
 }
